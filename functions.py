121be6b3a8010da5ca90d134cce4b08fd7ecc874
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-25 15:08:41 +0530

    function and recursion

---
+++ functions.py
@@ -0,0 +1,21 @@
+#function
+def sum(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+x=2
+y=5
+print(sum(x, y))
+print(sub(x, y))
+
+#recursion
+
+def fact(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact(n-1)
+    
+print(fact(4))

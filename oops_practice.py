a6060f8c7df22a922ac028dcf34c86b2a8b80957
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-28 22:34:21 +0530

    oops practice

--- oops_practice.py
+++ oops_practice.py
@@ -1,16 +1,40 @@
-# student class to print avg marks
+# # student class to print avg marks
 
-class Student:
-    def __init__(self, name, marks):
-        self.name=name
-        self.marks=marks
-    def avg(self):
-        a= sum(self.marks)/len(self.marks)
-        print(round(a, 2))
+# class Student:
+#     def __init__(self, name, marks):
+#         self.name=name
+#         self.marks=marks
+#     def avg(self):
+#         a= sum(self.marks)/len(self.marks)
+#         print(round(a, 2))
         
 
 
-s1 = Student("Rohit", [95,55,85])
+# s1 = Student("Rohit", [95,55,85])
 
-s1.avg()
+# s1.avg()
         
+# create account class with 2 attributes- balance and a/c no.
+# create method for debit, credit and printing the balance
+
+class Account:
+    def __init__(self, bal, acc_no, ):
+        self.bal= bal
+        self.acc_no = acc_no
+    def credit(self, amount):
+        self.bal+=amount
+        print(f"{amount} credited and available balance is {self.bal}")
+    def debit(self, amount):
+        self.bal-=amount
+        print(f"{amount} debited and available balance is {self.bal}")
+    
+acc = Account(10000, 12345)
+print(acc.bal)
+
+acc.credit(5000)
+print(acc.bal)
+
+acc.debit(2000)
+acc.debit(1000)
+
+print(acc.bal)

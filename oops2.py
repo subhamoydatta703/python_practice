0274ffd453699e2ee6f201ff5db492a9dc86f53b
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-02 21:22:09 +0530

    Inheritance

--- oops2.py
+++ oops2.py
@@ -2,11 +2,11 @@
 
 # In Python OOP (Object-Oriented Programming), the del keyword is used to delete objects, object properties (attributes), or variables.
 
-class Student:
-    def __init__(self, name):
-        self.name = name
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
 
-s1= Student("Rahul")
+# s1= Student("Rahul")
 # print(s1)
 # print(s1.name)
 
@@ -16,15 +16,15 @@ s1= Student("Rahul")
 
 # "__" before atribute_name -> the attribute becomes private(like): eg., __password
 
-class Account:
-    def __init__(self, acc_no, acc_pass):
-        self.acc_no=acc_no
-        self.__acc_pass=acc_pass
+# class Account:
+#     def __init__(self, acc_no, acc_pass):
+#         self.acc_no=acc_no
+#         self.__acc_pass=acc_pass
     
-    def Getpass(self):
-        return self.__acc_pass
+#     def Getpass(self):
+#         return self.__acc_pass
     
-a1= Account(12345, 7890)
+# a1= Account(12345, 7890)
 
 # When you use double underscore (__) before a variable name,
 # Python performs name mangling — it internally changes the variable’s name to:
@@ -34,7 +34,44 @@ a1= Account(12345, 7890)
 
 # So __acc_pass becomes private and cannot be directly accessed from outside the class.
 # print(a1.__acc_pass)
-print(a1.Getpass()) # best way to get the private data(like passwords) here we use encapsulation
+# print(a1.Getpass()) # best way to get the private data(like passwords) here we use encapsulation
+        
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+
+#     # convert methods into private -> the internal functions of this class can access this private method
+#     def __Greet(self):
+#         print("Hello")
+
+#     def Wlc(self):
+#         self.__Greet()
+
+# s1= Student("Rahul")
+
+# # print(s1.__Greet())
+# s1.Wlc() 
+
+# inheritance -> inherit the properties from parent class to child class
+
+# syntax -> another_class another_class_name(parent_class):
+
+class House:
+    def __init__(self):
+        self.windows = True
+        self.doors = True
+
+class Building(House):
+    def __init__(self):
+        # super().__init__() → calls House.__init__() → sets windows=True, doors=True
+        super().__init__()  # call House constructor
+        self.color= "blue"
+        
+b1 = Building()
+
+print(b1.doors)
+print(b1.color)
+
         
 
 

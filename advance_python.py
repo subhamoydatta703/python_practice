f486d5e29cf861e5817382271c4b8023a5068ec2
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-10 10:15:24 +0530

    Type definition(Type Alias)

--- advance_python.py
+++ advance_python.py
@@ -1,4 +1,15 @@
 # Walrus operator(:= )
 
-if(val:=int(input("enter a number: "))>5):
-    print("grater than 5")
+# if(val:=int(input("enter a number: "))>5):
+#     print("grater than 5")
+
+# type definition/alias(Using  : symbol after the variable and then write the data type)
+
+
+n: int = 5
+x: str = "Rahul"
+
+def sum(x: int, y: int) -> int:
+    return x+y
+
+print(sum(5,6))

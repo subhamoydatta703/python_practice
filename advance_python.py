b3434bdf5d711605e6989e9ab8642a721dc69e3d
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-10 10:40:38 +0530

    Type hints

--- advance_python.py
+++ advance_python.py
@@ -3,7 +3,7 @@
 # if(val:=int(input("enter a number: "))>5):
 #     print("grater than 5")
 
-# type definition/alias(Using  : symbol after the variable and then write the data type)
+# type hints(Using  : symbol after the variable and then write the data type)
 
 
 n: int = 5

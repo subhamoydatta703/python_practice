b28b86aeaed4a6a1bd59495ea5d69e60998c6cef
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-10 14:06:10 +0530

    Using map, filter and reduce

--- adv_py2.py
+++ adv_py2.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 # lambda function 
 
 sqr = lambda x: x**2
@@ -6,4 +8,28 @@ print(sqr(5))
 #.join()
 
 a = ["Rahul", "Rohan", "Ronit"]
-print("-".join(a))
+# print("-".join(a))
+
+# map(), filter(), reduce()
+
+lst = [1, 2, 3, 4]
+list_square = lambda x:x**2
+lst_sqr=list(map(list_square, lst))
+print(lst_sqr) #gives a new list with squared values of each elements of lst
+
+list_even = lambda x: x%2==0
+evens = list(filter(list_even, lst))
+# print(evens)
+
+def sum(a, b):
+    return a+b
+print(reduce(sum, lst))
+
+# working of reduce() ->
+# lst = [1, 2, 3, 4] 
+# dry run->
+# 1+2=3 then 3+3=6 then 6+4=10
+#final output of reduce() -> 10
+
+
+

e6b914b1c38ebd287b32dff55df25e9f9fb97b20
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-10 10:49:25 +0530

    Type definition/alias

--- advance_python.py
+++ advance_python.py
@@ -6,10 +6,22 @@
 # type hints(Using  : symbol after the variable and then write the data type)
 
 
-n: int = 5
-x: str = "Rahul"
+# n: int = 5
+# x: str = "Rahul"
 
-def sum(x: int, y: int) -> int:
-    return x+y
+# def sum(x: int, y: int) -> int:
+#     return x+y
 
-print(sum(5,6))
+# print(sum(5,6))
+
+# Type definition/alias
+
+type vector = list[float]
+
+
+def scale(s: float, v:vector)->vector:
+    return [s*num for num in v]
+
+v1: vector = [1.0, 2.0, 3.0]
+print(scale(2.0, v1))
+

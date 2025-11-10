960b164bf1a73c1c55275614544ef68a367e0172
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-10 12:15:25 +0530

    Using global

--- advance_python.py
+++ advance_python.py
@@ -72,4 +72,18 @@ ict1 = {"a": 1, "b": 2}
 dict2 = {"b": 3, "c": 4}
 
 dict1 |= dict2 #update the value of b in dict 1(dict2 overwrites the value of b in dict 1)
-print(dict1)
+# print(dict1)
+
+#global
+
+a= 40
+
+def func():
+    global a # global -> changes the original value of global variables
+    a = 5
+    print(a)
+
+# func() 
+
+
+

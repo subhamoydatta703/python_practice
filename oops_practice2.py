a951d53dbdf459e12fe15a21aa285102d002b2b5
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-12 12:03:09 +0530

    Basic opps practice

--- oops_practice2.py
+++ oops_practice2.py
@@ -4,9 +4,30 @@ class Programmer:
         self.name= name
         self.lan = lan
         self.sal = sal
+
+
+class Calculator:
+    def __init__(self, num):
+        self.num=num
+    def sq(self):
+        return self.num**2
+    def cube(self):
+        return self.num**3
+    def roots(self):
+        return round(self.num**0.5, 2)
+    @staticmethod
+    def greet():
+        print("Hello")
+
 p1 = Programmer("Rahul", "Python", 2500000)
 p2 = Programmer("Rohit", "Java", 2500000)
 p3 = Programmer("Ronit", "Js", 1800000)
-print(p1.company)
-print(p2.lan)
-print(p3.sal)
+# print(p1.company)
+# print(p2.lan)
+# print(p3.sal)
+
+n = Calculator(5)
+print(n.sq()) 
+print(n.cube()) 
+print(n.roots()) 
+n.greet()

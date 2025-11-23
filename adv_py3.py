a50a33985d64abaacd62d015257d2a7ee8ec40f7
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-23 14:08:38 +0530

    Using itertools module

--- adv_py3.py
+++ adv_py3.py
@@ -1,3 +1,16 @@
 from collections import Counter 
+import itertools
+
 # count the number of each character
 print(Counter("Subhamoy Datta  "))
+
+
+
+# Combination: selecting items where order does not matter (e.g., AB = BA)
+print(list(itertools.combinations([7,8,9,10],3))) 
+
+
+# Permutation: selecting items where order matters (e.g., AB ≠ BA)
+print(list(itertools.permutations([7,8,9,10],3))) 
+
+
